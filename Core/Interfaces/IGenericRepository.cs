6da8dca774f7e1b24c4c10d278e1efee710ae561
Sharif81 <sharifahamedalif@gmail.com>
2026-01-20 13:52:39 +0600

    End Of Section 4

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T?> GetEntityWithSepc(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<TResult?> GetEntityWithSepc<TResult>(ISpecification<T, TResult> spec);
+        Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T, TResult> spec);       
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<bool> SaveAllAsync();
+        bool Exits(int id);
+    }
+}
---
+++ Core/Specifications/BaseSpecifications.cs
@@ -0,0 +1,51 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class BaseSpecifications<T>(Expression<Func<T, bool>>? criteria) : ISpecification<T>
+    {        
+        protected BaseSpecifications() : this(null)
+        {
+
+        }
+        public Expression<Func<T, bool>>? Criteria => criteria;
+
+        public Expression<Func<T, object>>? OrderBy { get; private set; }
+
+        public Expression<Func<T, object>>? OrderByDescending {get; private set; }
+
+        public bool IsDistinct { get; private set; }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression) 
+        {
+            OrderBy = orderByExpression;
+        }
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyDistinct()
+        {
+            IsDistinct = true;
+        }
+    }
+
+   public class BaseSpecifications<T, TResult>(Expression<Func<T, bool>> criteria) : BaseSpecifications<T>(criteria), ISpecification<T, TResult>
+    {      
+        protected BaseSpecifications() : this(null!)
+        {
+        }
+        public Expression<Func<T, TResult>>? Select { get; private set; }
+        protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
+        {
+            Select = selectExpression;
+        }
+    }
+}
