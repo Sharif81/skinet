e8969b631af76de7c55f906b6257273f7c0fa4b1
Sharif81 <sharifahamedalif@gmail.com>
2026-05-02 17:54:56 +0600

    End Of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,14 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
-  private http = inject(HttpClient)
-  title = 'Skinet'
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-      next: Response => this.products = Response.data, 
-      error: error => console.log(error),
-      complete: () => console.log('complete')
-    })
-  }
+export class AppComponent  {
+  title = "Skinet";
 }
